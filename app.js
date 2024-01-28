9d9196658caced1f33cab149e51d4a3cd25edba6
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-01-28 14:04:02 +0200

    burger activation

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let controller;
 let slideScene;
 let pageScene;
-let mouse = document.querySelector(".cursor");
-let mouseTxt = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
+
+// event listeners
+burger.addEventListener("click", navToggle);
+window.addEventListener("mouseover", activeCursor);
+window.addEventListener("mousemove", cursor);
 
 function animateSlides() {
   // init Controller
@@ -83,7 +89,22 @@ function activeCursor(e) {
   }
 }
 
-window.addEventListener("mouseover", activeCursor);
-window.addEventListener("mousemove", cursor);
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: 45, y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: -45, y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: 0, y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: 0, y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
 
 animateSlides();
